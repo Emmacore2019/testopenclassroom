22cbdd31d48dac4863d32209a4462f93a6b37887
Emmanuel Moussi <emmanuel.moussi@corpecca.africa>
2019-01-04 17:06:36 +0100

    deuxieme fichier modifie

--- hellozero.py
+++ hellozero.py
@@ -1,12 +1,4 @@
- Le volume dans le lecteur C s'appelle OS
- Le num‚ro de s‚rie du volume est B2DC-775D
-
- R‚pertoire de C:\Users\emmanuel\Documents\emmagit\projetpython\testopenclassroom
-
-04/01/2019  17:03    <DIR>          .
-04/01/2019  17:03    <DIR>          ..
-04/01/2019  17:03                 0 hellozero.py
-04/01/2019  17:01                86 hellozero.txt
-04/01/2019  16:57                19 README.md
-               3 fichier(s)              105 octets
-               2 R‚p(s)  833ÿ411ÿ325ÿ952 octets libres
+"""
+fichier hello pour moi le zero sur git. formation 
+interressante
+"""
